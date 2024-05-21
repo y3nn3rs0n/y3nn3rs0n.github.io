fd2f488d97088f22b4e629bab44f36440e1ff861
yennerson <yennerson.olivo@gmail.com>
2024-05-20 23:34:13 -0500

    all sections created

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-// js/main.js
-function loadComponent(url, elementId) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(elementId).innerHTML = data;
-        });
-}
+let selections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-document.addEventListener("DOMContentLoaded", function() {
-    loadComponent('navBar/navbar.html', 'navbar');
-    loadComponent('/body/body.html', 'main-content');
-});
+window.onscroll = () => {
+    selections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+
+    /* navbar */
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);}
