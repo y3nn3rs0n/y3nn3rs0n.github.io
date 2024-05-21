98bf47efb7a39b17fc7ed7f459bae764352fd1c8
yennerson <yennerson.olivo@gmail.com>
2024-05-21 14:21:20 -0500

    all sections created

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+/*TOGGLE ICON NAVBAR*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+/*scroll active link*/
 let selections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -14,9 +24,15 @@ window.onscroll = () => {
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
         }
-    })
+    });
 
-    /* navbar */
+    /* navbar sticky */
 
     let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);}
+    header.classList.toggle('sticky', window.scrollY > 100);
+   /* remove toggle icon and navbar when click navbar link */
+
+   menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
